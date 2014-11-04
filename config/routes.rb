a001626800cdb6ebb53673aa770642a69d298b54
afisch710 <alexander-fischman@uiowa.edu>
2014-11-04 16:24:39 -0400

    setting up charity controller, model, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tenkhours::Application.routes.draw do
   resources :home
   resources :users
+  resources :charities
   match '/login', to: 'sessions#new', via: :get
   match '/login_create', to: 'sessions#create', via: :post
   match '/logout', to: 'sessions#destroy', via: :delete
