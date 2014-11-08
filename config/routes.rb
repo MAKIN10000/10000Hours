19fa419e4b153ff5667737a0f2e4075fe8bc52cb
ibrauer <ian-brauer@uiowa.edu>
2014-11-07 22:13:42 -0400

    added the goals paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Tenkhours::Application.routes.draw do
   resources :home
   resources :users
   resources :charities
+  resources :goals
   match '/login', to: 'sessions#new', via: :get
   match '/login_create', to: 'sessions#create', via: :post
   match '/logout', to: 'sessions#destroy', via: :delete
