59d9367e62b769bc76e48645203b54abce9083ae
mjayjones13 <michael-jones@uiowa.edu>
2014-11-17 14:17:45 -0400

    comment out line

--- app/mailers/user_notifier.rb
+++
@@ -1,9 +0,0 @@
-class UserNotifier < ActionMailer::Base
-  default :from => "10khourbot@gmail.com"
-  
-  def send_signup_email(user)
-  	@user = user
-  	mail(:to => @user.email,
-  	:subject => "Thank you for signing up with us!")
-  end
-end
