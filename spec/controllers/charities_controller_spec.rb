7f45bb95bc641322cac6d0c3fb6e4b0aafc1de02
afisch710 <alexander-fischman@uiowa.edu>
2014-11-18 01:55:33 -0400

    added spec testing for charities controller

---
+++ spec/controllers/charities_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe CharitiesController do
+  it 'should select the home page to render' do
+      post :create, :charity => {:name => "charity_for_a_test", :description => "test", :president_name => "tester", :contact_email => "testies@test.com", :charity_website => "https://www.test.com" }
+      expect(response).to redirect_to('/home')
+  end
+
+  describe 'Creating a charity that already exists' do
+    before(:all) do
+      movieHash = {:name => "test123", :description => "test", :president_name => "tester", :contact_email => "test1234567890987654321@test.com", :charity_website => "https://www.test.com" }
+      Charity.create!(movieHash)
+      charities_with_that_name = Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").count
+      if (charities_with_that_name > 0)
+        Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").destroy
+      end
+    end
+
+    it 'should not be created in the database' do
+      post :create, :charity => {:name => "xxsaeignn__bbnannea842mmr3920wllx69", :description => "test", :president_name => "tester", :contact_email => "test1234567890987654321@test.com", :charity_website => "https://www.test.com" }
+
+      expect(Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").count).to equal(0)
+    end
+    after(:all) do
+      Charity.where(:contact_email => "test1234567890987654321@test.com").destroy
+    end
+
+  end
+
+  describe 'Creating a charity that does not already exist' do
+    before(:all) do
+      charities_with_that_name = Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").count
+      if (charities_with_that_name > 0)
+        Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").destroy
+      end
+    end
+
+    it 'should add the charity to the database' do
+      post :create, :charity => {:name => "xxsaeignn__bbnannea842mmr3920wllx69", :description => "test", :president_name => "tester", :contact_email => "test12345678909876543210@test.com", :charity_website => "https://www.test.com" }
+
+      expect(Charity.where(:name => "xxsaeignn__bbnannea842mmr3920wllx69").count).to equal(1)
+
+    end
+
+  end
+
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'simplecov'
-SimpleCov.start 'rails'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -30,18 +28,14 @@ RSpec.configure do |config|
     #   # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    load "#{Rails.root}/db/seeds.rb"
-    OmniAuth.config.test_mode = true
-  end
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
-    mocks.verify_partial_doubles =false 
+    mocks.verify_partial_doubles = true
   end
 
 # The settings below are suggested to provide a good initial experience
