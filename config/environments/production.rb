8cb356c10b005aa57acb6804bbcc7afd9252ea65
nmartinson <nick-martinson@uiowa.edu>
2014-11-23 20:07:22 -0400

    Added email config info to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,17 @@ Tenkhours::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.smtp_settings = {	
+						:address							=>	"smtp.gmail.com",
+						:port									=>	587,
+						:domain								=>	'gmail.com',
+						:user_name						=>	'10khourbot@gmail.com',
+						:password							=>	'deadline13',
+						:authentication				=>	'plain',
+						:enable_starttls_auto	=>	true		}
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true 
 
