e2e160833bdce52de68dd576165da2800ebae21b
nmartinson <nick-martinson@uiowa.edu>
2014-12-06 20:20:29 -0400

    Added a totalpledge column to Charities model. Added email functionaily for reminding a user if a goal is about to end.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124221950) do
+ActiveRecord::Schema.define(version: 20141206235242) do
 
   create_table "charities", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141124221950) do
     t.string   "charity_website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "totalPledge"
   end
 
   create_table "goals", force: true do |t|
