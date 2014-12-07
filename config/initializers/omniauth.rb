2e43eea3b58a8c291a59355eb11b0067c70aac9a
kalbozo <kaleb-bozorgzadeh@uiowa.edu>
2014-12-07 15:23:25 -0600

    POST TO FACEBOOK on goal creation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, 662977337148851, "ee071a0fba69531b03d0f72b1832cfe4",
-  :scope => 'email,public_profile,user_friends',:display => 'popup'
+  :scope => 'email,public_profile,user_friends,publish_actions,manage_pages,status_update'
 end
