58d382f7f7bdc3a0dc5720999bf2599ec1aa8a7f
nmartinson <nick-martinson@uiowa.edu>
2014-12-08 22:49:07 -0400

    Added spec for goal controller

---
+++ spec/controllers/goals_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe GoalsController do
+  describe 'Completion' do
+    context 'Goal is completed'
+    it 'should render index page' do
+      expect(Goal).to receive(:find)
+      #put :update, :id=>{:user_id => 1, :title=> "HEEEY",:description => "HI", :pledge_amount => 3.5, :charity_id => 3}
+      expect(response).to redirect_to('/')
+    end
+  end
+end
+
