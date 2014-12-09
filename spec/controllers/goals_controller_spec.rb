43a52a45241377d036e37f19029e31eed059e885
nmartinson <nick-martinson@uiowa.edu>
2014-12-08 23:08:00 -0400

    Removed a puts from the goal controller

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -6,7 +6,7 @@ describe GoalsController do
     it 'should render index page' do
       expect(Goal).to receive(:find)
       #put :update, :id=>{:user_id => 1, :title=> "HEEEY",:description => "HI", :pledge_amount => 3.5, :charity_id => 3}
-      expect(response).to redirect_to('/')
+      expect(response).to redirect_to('/home')
     end
   end
 end
